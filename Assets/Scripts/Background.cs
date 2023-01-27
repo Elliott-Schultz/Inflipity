63e26df065346647e214c89a40365ed0c39a02ef
waowens2 <waowens2@ncsu.edu>
2023-01-27 14:40:32 -0500

    Initial BG setup
    Need to write script for movement, random spawns & sizes

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public Vector2[] spawnPoints;
+    public GameObject[] bgObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
